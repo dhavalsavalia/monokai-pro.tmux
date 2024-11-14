5dcfece094dd047d36f2859d8d1f12640279a101
Dhaval Savalia <hello@dhavalsavalia.com>
2024-11-14 15:04:43 -0500

    add session name

--- modules/logo.sh
+++ modules/logo.sh
@@ -4,7 +4,7 @@ show_logo() {
   local color="$dark2"
 
   local show_left_separator="#[fg=$background,bg=$dark2,nobold,nounderscore,noitalics]$status_left_separator"
-  local show_text="#[fg=$color,bg=$background,bold]$logo"
+  local show_text="#[fg=$color,bg=$background,bold]$logo #S"
   local show_right_separator="#[fg=$background,bg=$dark2,nobold,nounderscore,noitalics]$status_right_separator"
   echo "$show_left_separator$show_text$show_right_separator"
 }
